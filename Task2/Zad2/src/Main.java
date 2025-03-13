a47a6de4d4c1e1baa8a20f317717b028ce51cf56
Mikołaj Sienicki <s33617@pjwstk.edu.pl>
2025-03-13 13:48:05 +0100

    Skonczone Zadanie

--- Task2/Zad2/src/Main.java
+++ Task2/Zad2/src/Main.java
@@ -1,5 +1,37 @@
+import com.sun.source.doctree.EscapeTree;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int cena = 40;cd
+        int cenastartowa = 40;
+        int znizka = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj wiek:");
+        int wiek = sc.nextInt();
+        System.out.println("Podaj miejsce zamieszkania");
+        String miejsce = sc.next();
+        System.out.println("Podaj na kiedy kupujesz bilet");
+        String dzien = sc.next();
+
+        if (dzien.equalsIgnoreCase("Czwartek")) {
+            znizka = 100;
+        }
+        if (miejsce.equalsIgnoreCase("Warszawa")) {
+            znizka = znizka + 10;
+        }
+        if (wiek < 10){
+            znizka = znizka + 100;
+        }
+        if (wiek >= 10 && wiek <18){
+            znizka = znizka + 50;
+        }
+        if (znizka > 100) {
+            System.out.println("Bilet jest darmowy");
+        } else {
+            int nowacena = cenastartowa*((100-znizka)/100);
+            System.out.println("Cena biletu wynosi: " + nowacena);
+        }
+
     }
 }
